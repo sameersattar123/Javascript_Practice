05b0faf3b3c908349a7dc82b56d1e49066bdcc27
Sameer <sameersattar1111@gmail.com>
2023-11-26 23:31:46 +0500

    comparisons

---
+++ Basics/Comparisons.js
@@ -0,0 +1,17 @@
+// console.log(2 == 3);
+// console.log(2 >= 3);
+// console.log(2 <= 3);
+// console.log(2 != 3);
+
+// console.log("2" > 1); // string  convert into number
+// console.log("02" < 1); // string  convert into number
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // null convert into number , that is why it is true
+
+console.log(undefined > 0); // false
+console.log(undefined == 0); // false
+console.log(undefined <= 0); // false
+
+console.log("2" === 2);
