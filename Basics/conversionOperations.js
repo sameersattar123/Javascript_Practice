d0c8dd8de2c28abf8079b71c869bdb4cced748e4
Sameer <sameersattar1111@gmail.com>
2023-11-26 07:49:49 +0500

    conversions

--- Basics/conversionOperations.js
+++ Basics/conversionOperations.js
@@ -1,23 +1,60 @@
 let str = "334nk";
 
-console.log(str)
-console.log(typeof str)
+// console.log(str)
+// console.log(typeof str)
 
 let numIntoNum = Number(str);
-console.log(numIntoNum);
-console.log(typeof numIntoNum);
+// console.log(numIntoNum);
+// console.log(typeof numIntoNum);
 
 // "33" ==> 33 
 // "33avf" ==> NaN
 // true =>  1 , false => 0
 
 let isLogged = ""
-console.log(isLogged);
+// console.log(isLogged);
 
 let booleanIslogged = Boolean(isLogged);
-console.log(booleanIslogged)
-console.log(typeof booleanIslogged)
+// console.log(booleanIslogged)
+// console.log(typeof booleanIslogged)
 
 // 1 => true , 1 => false 
 // " " =>  false
 // "sameer" => 1 
+
+let num2 = 44;
+// console.log(num2)
+// console.log(typeof num2)
+
+let num2IntoStr  = String(num2);
+// console.log(num2IntoStr)
+// console.log(typeof num2IntoStr)
+
+// *************** Operations *********************
+
+let value = 9;
+let  negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+// console.log(2 ** 2);
+
+// let str1 = "wasey";
+// let str2 = " khan"
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(2 + "5")
+// console.log("2" + 5)
+// console.log(2 + 3 + "4"); // operation decide  from first letter if it is number then all numbers
+// console.log("4" + 3 + 2); // operation decide  from first letter if it is str then all strings
+
+let gameCounter = 22;
+++gameCounter; // infix
+gameCounter++; // postfix
+console.log(gameCounter)
