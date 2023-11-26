8486ce5db97600bbb45300411b2adea873ebc3e5
Sameer <sameersattar1111@gmail.com>
2023-11-26 07:13:46 +0500

    Basics

---
+++ Basics/conversionOperations.js
@@ -0,0 +1,23 @@
+let str = "334nk";
+
+console.log(str)
+console.log(typeof str)
+
+let numIntoNum = Number(str);
+console.log(numIntoNum);
+console.log(typeof numIntoNum);
+
+// "33" ==> 33 
+// "33avf" ==> NaN
+// true =>  1 , false => 0
+
+let isLogged = ""
+console.log(isLogged);
+
+let booleanIslogged = Boolean(isLogged);
+console.log(booleanIslogged)
+console.log(typeof booleanIslogged)
+
+// 1 => true , 1 => false 
+// " " =>  false
+// "sameer" => 1 
