e57b145031a325fe09fab41afa8699e3d2ab09ae
Sameer <sameersattar1111@gmail.com>
2023-11-30 14:50:48 +0500

    objects

--- Basics/Objects.js
+++ Basics/Objects.js
@@ -1,1 +1,29 @@
-console.log("sameer")
+const mySymbol = Symbol("key1");
+
+const myObj = {
+    [mySymbol] : "sameer",
+    name : "sameer",
+    isLogged : false,
+    friends : ["ali" , "gaffar"],
+    gmail : "sameer@gmail.com"
+}
+
+console.log(myObj.friends)
+console.log(myObj["friends"])
+console.log(myObj["gmail"])
+console.log(myObj["isLogged"])
+console.log(myObj["name"])
+console.log(myObj[mySymbol]);
+console.log(typeof myObj[mySymbol]);
+console.log(myObj)
+
+myObj.greeting = function(){
+    console.log("Hello")
+}
+myObj.greetingTwo = function(){
+    console.log(`my name is ${this.name}`)
+}
+
+console.log(myObj.greeting())
+console.log(myObj.greetingTwo())
+console.log(myObj)
