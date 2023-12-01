b32a0c02af484d7c5ade4056e159dafd363675a2
Sameer <sameersattar1111@gmail.com>
2023-12-01 15:27:51 +0500

    scopes

--- Basics/scopes.js
+++ Basics/scopes.js
@@ -19,3 +19,40 @@ console.log("Outer c " , c);
 console.log("Outer a " , a);
 console.log("Outer b " , b);
 console.log("Outer c " , c);  
+
+
+// closures
+
+function one(){
+    const name = "sameer";
+    function two(){
+        const website = "youtube"
+        console.log(name)
+    }
+    // console.log(website) // out of scope , we cannot used any keyword
+    two();
+}
+one();
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+     // console.log(website); // out of scope , we cannot used any keyword
+}
+
+// console.log(username);
+
+// ************************ Interesting mini concept of hositing **********************
+
+console.log(addOne(2)); // 3
+function addOne(num){
+    return num + 1;
+}
+
+console.log(addTwo(4)); // Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num + 2;
+}
