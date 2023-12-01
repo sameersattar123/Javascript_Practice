cb573bf315be25d6278ff244d9a88164622d4aa5
Sameer <sameersattar1111@gmail.com>
2023-12-01 14:52:58 +0500

    global block scope

--- Basics/scopes.js
+++ Basics/scopes.js
@@ -1,1 +1,21 @@
-console.log("sameeer");
+let a = 10;
+const b = 20;
+var c = 30;
+
+console.log("Outer a " , a);
+console.log("Outer b " , b);
+console.log("Outer c " , c);
+
+{
+    let a = 880;
+    console.log("Inner a " , a);
+    const b = 9990;
+    console.log("Inner b " , b);
+    var c = 40;
+    console.log("Inner b " , c);
+
+}
+
+console.log("Outer a " , a);
+console.log("Outer b " , b);
+console.log("Outer c " , c);
