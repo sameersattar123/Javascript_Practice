4354c44bb17f95def53e890088efe2bec6f0523a
Sameer <sameersattar1111@gmail.com>
2023-12-02 18:46:05 +0500

    switch cases

---
+++ Basics/controlFlow/switch.js
@@ -0,0 +1,27 @@
+// switch case syntax 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 2;
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Febrary")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    default:
+        console.log("default case month")
+        break;
+}
