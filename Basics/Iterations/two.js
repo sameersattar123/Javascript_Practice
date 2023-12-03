8ded9ab6c275ebaed2c3b81936c4af18dfaf641c
Sameer <sameersattar1111@gmail.com>
2023-12-03 11:20:18 +0500

    while do while loop

---
+++ Basics/Iterations/two.js
@@ -0,0 +1,20 @@
+let index = 1;
+
+while (index <= 10) {
+    console.log(index);
+    index++;
+}
+
+let myArray = ["sami" , "ali" , "faraz" , "kashif" , "sara"];
+let name = 0;
+while ( name < myArray.length) {
+    console.log(myArray[name])
+    name =name + 1;
+}
+
+let i = 10;
+
+do {
+ console.log(i)
+ i--;   
+} while (i >= 1);
