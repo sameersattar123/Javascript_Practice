34b8ab1308d7318a68024a2f17d873743bc7e075
Sameer <sameersattar1111@gmail.com>
2023-12-06 12:21:13 +0500

    guess Number

---
+++ Projects/GuessNumber/index.js
@@ -0,0 +1,44 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector("#subt")
+
+const userInput = document.querySelector("#guessFeild")
+
+const  guessSlot = document.querySelector(".guessess")
+const remaining = document.querySelector(".LastResult")
+
+const lowOrHi = document.querySelector(".LowOrHi");
+
+const startOver = document.querySelector(".resultParas")
+
+let p = document.createElement('p');
+
+let prevGuess = [];
+
+let numGuess = 1 ;
+
+let playGame  = true;
+
+function validateGuess(guess){
+    
+}
+
+function checkGuess(guess){
+
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+
+}
+
+function endGame(){
+
+}
+
+function startGame(){
+
+}
