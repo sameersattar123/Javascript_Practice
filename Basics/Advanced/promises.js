0ea4c2eceeffe55e2febc7325b89af2aacb36a7e
Sameer <sameersattar1111@gmail.com>
2023-12-08 16:38:23 +0500

    promises and fetch

--- Basics/Advanced/promises.js
+++ Basics/Advanced/promises.js
@@ -47,11 +47,67 @@ const promisedFour = new Promise((resolve, reject) => {
   }, 1000);
 });
 
-promisedFour.then((user) => {
-  console.log(user);
-  return user.email
-}).then((userEmail) => {
-    console.log(userEmail)
-}).catch((error) => {
+promisedFour
+  .then((user) => {
+    console.log(user);
+    return user.email;
+  })
+  .then((userEmail) => {
+    console.log(userEmail);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => console.log("The promise is either resolved or rejected"));
+
+const promisedFive = new Promise((resolve, reject) => {
+  let error = false;
+  setTimeout(() => {
+    if (!error) {
+      resolve({
+        langName: "Javascript",
+        framework: "REACT JS ",
+      });
+    } else {
+      reject("ERROR: Something went wrong");
+    }
+  }, 1000);
+});
+
+async function consumedFive() {
+  try {
+    const response = await promisedFive;
+    console.log(response);
+  } catch (error) {
     console.log(error)
-}).finally(() => console.log("The promise is either resolved or rejected"))
+  }
+}
+
+consumedFive();
+
+async function getAllUser() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// getAllUser();
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+
+consumedFive();
+
+
+
