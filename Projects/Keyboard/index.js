df89e63c7d3cabf633559d27ded309dded6db382
Sameer <sameersattar1111@gmail.com>
2023-12-08 22:56:17 +0500

    keyboard

--- Projects/Keyboard/index.js
+++ Projects/Keyboard/index.js
@@ -1,1 +1,18 @@
-console.log("sameer sattar")
+const insert = document.getElementById("insert")
+
+window.addEventListener("keydown" , (e) => {
+    insert.innerHTML = `
+  <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    `
+})
