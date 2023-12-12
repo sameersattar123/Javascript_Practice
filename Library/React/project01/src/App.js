f3cce8f23a06b192ba865dcf1262814e973320d2
Sameer <sameersattar1111@gmail.com>
2023-12-12 19:46:19 +0500

    new component

--- Library/React/project01/src/App.js
+++ Library/React/project01/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Chai from './Chai'
 
 const App = () => {
   return (
     <div>
+      <Chai/>
       sameer sattar
     </div>
   )
---
+++ Library/React/project01/src/Chai.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chai = () => {
+  return (
+    <div>Chai</div>
+  )
+}
+
+export default Chai
