94132965ea86470f221e227529c61f22e444ffa6
Sameer <sameersattar1111@gmail.com>
2023-12-15 00:32:28 +0500

    currency converter

--- Library/React/project01/src/components/Hooks/useCurrencyInfo.js
+++ Library/React/project01/src/components/Hooks/useCurrencyInfo.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
 
-  useEffect(async () => {
+  useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
     )
