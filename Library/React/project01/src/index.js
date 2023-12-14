88f1f517b71c619dbe64595fa056f2127cd9bce6
Sameer <sameersattar1111@gmail.com>
2023-12-15 01:36:05 +0500

    context api

--- Library/React/project01/src/index.js
+++ Library/React/project01/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css"
+import UserContextProvider from './components/Context/UserContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <UserContextProvider>
     <App />
-  </React.StrictMode>
+  </UserContextProvider>
 );
 
