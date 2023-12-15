69b35489cb2cae4415e7e8e6afe34f05381796f4
Sameer <sameersattar1111@gmail.com>
2023-12-15 23:47:59 +0500

    todo form

--- Library/React/project01/src/components/TodoForm.jsx
+++ Library/React/project01/src/components/TodoForm.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { useTodo } from './Context/TodoContext';
 
 function TodoForm() {
+
+    const [todo, setTodo] = useState("")
+
+    const {addTodo} = useTodo();
+
+    const add = (e) => {
+        e.preventDefault();
+
+        if (!todo) {
+            return;
+        } else {
+            addTodo({complete : false , todo })
+            setTodo("")
+        }
+    }
     
 
     return (
-        <form  className="flex">
+        <form onSubmit={add}  className="flex">
             <input
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
                 type="text"
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
