4e67e878bb9b5724f8c65a8765fb1583c2b4b4e5
Sameer <sameersattar1111@gmail.com>
2023-12-16 18:56:13 +0500

    redux toolkit project

---
+++ Library/React/project01/src/Features/Todo/TodoSlice.js
@@ -0,0 +1,33 @@
+import { createSlice , nanoid } from "@reduxjs/toolkit"
+
+const initialState = {
+    todos : [
+        {
+
+        }
+    ]
+}
+
+export const todoSlice = createSlice({
+    name : "todo",
+    initialState,
+    reducers : {
+        add_Todo : (state , action) =>{
+        const todo = {
+            id : nanoid(),
+            text : action.payload
+        }
+        state.todos.push(todo)
+        },
+        remove_Todo : (state , action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        update_todo : (state , action) => {
+
+        }
+    }
+})
+
+export const {add_Todo , update_todo ,remove_Todo} = todoSlice.actions
+
+export default todoSlice.reducer
