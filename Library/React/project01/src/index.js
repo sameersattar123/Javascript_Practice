6a0ef8909a762f14a201d28e80480cf74659649a
Sameer <sameersattar1111@gmail.com>
2023-12-18 07:15:55 +0500

    routing

--- Library/React/project01/src/index.js
+++ Library/React/project01/src/index.js
@@ -5,38 +5,54 @@ import "./index.css"
 import UserContextProvider from './components/Context/UserContextProvider';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Home from './components/Website/Home';
 import Layout from './components/Layout';
 import About from './components/Website/AboutUs';
 import Contact from './components/Website/ContactUs';
+import User from './components/Website/User';
 
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element : <Layout/>,
-    children : [
-      {
-        path: "",
-        element : <Home/>
-      },
-      {
-        path: "about",
-        element : <About/>
-      },
-      {
-        path: "contact",
-        element : <Contact/>
-      },
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path : "/",
+//     element : <Layout/>,
+//     children : [
+//       {
+//         path: "",
+//         element : <Home/>
+//       },
+//       {
+//         path: "about",
+//         element : <About/>
+//       },
+//       {
+//         path: "contact",
+//         element : <Contact/>
+//       },
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+    <Route path='' element={<Home/>}></Route>
+    <Route path='about' element={<About/>}></Route>
+    <Route path='contact' element={<Contact/>}></Route>
+    <Route path='user/:userid' element={<User/>}></Route>
+    </Route>
+  )
+)
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <UserContextProvider>
     <Provider store={store}>
     <RouterProvider router={router} />
+    <App/>
     </Provider>
   </UserContextProvider>
 );
