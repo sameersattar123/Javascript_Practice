a5dd2451025352c9c5a02d41387ecc6919870e36
Sameer <sameersattar1111@gmail.com>
2024-01-05 01:49:54 +0500

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,19 @@
-console.log("sameer")
+const express = require("express")
+const app = express();
+
+const port = 4000;
+
+app.get("/" , (req , res) => {
+    res.send("Hello World")
+})
+app.get("/sameer" , (req , res) => {
+    res.send("Hello World sameer")
+})
+
+app.get("/chai" , (req , res) => {
+    res.send("<h1>Hello World chai</h1>")
+})
+
+app.listen(port , () => {
+    console.log(`Example app listening on port ${port}`) 
+})
