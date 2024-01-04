17e28b48e593923db59e5e82b9b794211be21dfa
Sameer <sameersattar1111@gmail.com>
2024-01-05 01:56:33 +0500

    env file

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,6 @@
+require("dotenv").config()
 const express = require("express")
 const app = express();
-
-const port = 4000;
 
 app.get("/" , (req , res) => {
     res.send("Hello World")
@@ -14,6 +13,6 @@ app.get("/chai" , (req , res) => {
     res.send("<h1>Hello World chai</h1>")
 })
 
-app.listen(port , () => {
-    console.log(`Example app listening on port ${port}`) 
+app.listen(process.env.PORT , () => {
+    console.log(`Example app listening on port`) 
 })
