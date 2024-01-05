4bb4c7b50fd7c22f922fc450414a104043eaa86e
Sameer <sameersattar1111@gmail.com>
2024-01-06 04:29:58 +0500

    modals

--- Backend/modals/hospitalManagement/doctor.modals.js
+++ Backend/modals/hospitalManagement/doctor.modals.js
@@ -1,7 +1,30 @@
 import mongoose from "mongoose";
 
 const DoctorSchema = new mongoose.Schema(
-  {},
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: String,
+      required: true,
+    },
+    qualification: {
+      type: String,
+      required: true,
+    },
+    experienceInYears: {
+      type: Number,
+      default: 0,
+    },
+    workInHospitals: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital",
+      },
+    ],
+  },
   {
     timestamps: true,
   }
