7b609a33711cd452be8d22a50516d4a56590ce24
Sameer <sameersattar1111@gmail.com>
2024-01-06 03:39:11 +0500

    modals

--- Backend/modals/ecommerce/order.modals.js
+++ Backend/modals/ecommerce/order.modals.js
@@ -1,8 +1,38 @@
 import mongoose from "mongoose";
 
+const orderItemSchema = new mongoose.Schema({
+    productId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Product"
+    },
+    quantity : {
+        type : Number, 
+        required : true
+    }
+})
+
 const OrderSchema = new mongoose.Schema(
     {
-
+        orderPrice : {
+            type :  Number,
+            required : true
+        },
+        customer : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        },
+        orderItems : {
+            type : [orderItemSchema]
+        },
+        address : {
+            type : String,
+            required : true
+        },
+        status : {
+            type :String,
+            enum : ["PENDING" , "CANCELLED" , "DELIEVERED"],
+            default : "PENDING"
+        }
     },
     {
         timestamps : true
