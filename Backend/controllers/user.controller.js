456de3c0707bfbbf69c2fcdedf15893d9b765859
Sameer <sameersattar1111@gmail.com>
2024-01-10 23:26:46 +0500

    user controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,9 +1,7 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message : "OK"
-     })
+   
 })
 
 
