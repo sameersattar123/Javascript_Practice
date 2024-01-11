9eeeb179a9a6ed9c500814865ddf5f750946c7c9
Sameer <sameersattar1111@gmail.com>
2024-01-11 22:20:26 +0500

    some changes

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,8 +1,21 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler(async (req,res) => {
-   
+         // *** Algorithm for Register a user **** 
+         // get user details from frontend 
+         // validation - not empty
+         // check if user already exits: username / email
+         // check for images , check for avatar
+         // upload them to cloudinary , avatar
+         // create user creation -  create entry in db
+         // remove password and refresh token from response
+         // check for user creation
+         // return response   
+
+        const {fullname , username , email , password } = req.body 
+      //   console.log("email: " , email)
+      //   console.log("password: " , password)
 })
 
 
-export {registerUser}
+export {registerUser} 
--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,11 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
  
-dotenv.config(); 
+dotenv.config();  
 
 connectDB() 
 .then(() => {
-    app.listen(process.env.PORT || 3000 , () => {  
+    app.listen(process.env.PORT || 4000 , () => {  
     console.log(`Server is running at port ${process.env.PORT}`)  
 }) 
 })   
