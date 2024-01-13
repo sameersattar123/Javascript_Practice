41be7a317c595ff299c00df54f270c3c22a90492
Sameer <sameersattar1111@gmail.com>
2024-01-14 00:21:30 +0500

    cloudinary

--- Backend/utils/cloudinary.js
+++ Backend/utils/cloudinary.js
@@ -18,6 +18,7 @@ const uploadOnCloudinary = async(localFilePath) => {
 
         // file has been uploaded successfully
         console.log("File is uploaded on cloudinary: " , response.url)
+        fs.unlinkSync(localFilePath)
         return response
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
