142bf558e76d14c199a3ba4b67a399efc360c370
Sameer <sameersattar1111@gmail.com>
2024-01-31 04:07:47 +0500

    logout user

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -160,7 +160,35 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const  logoutUser = asyncHandler(async ( res, req , next) => {
-  
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset : {
+        refreshToken : undefined
+      }
+    },
+    {
+      new : true
+    }
+  )
+
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",  options)
+    .json(
+      ApiResponse(
+        200,
+        {},
+        "User logged Out"
+      )
+    );
 })
 
 export { registerUser, loginUser , logoutUser };
