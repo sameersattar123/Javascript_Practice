35adf1c758dd4cd3ff499bd5bab5744d6a5ef0a9
Sameer <sameersattar1111@gmail.com>
2024-02-06 01:43:28 +0500

    controllers

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -219,8 +219,8 @@ const refreshAccessToken = asyncHandler(async (req, res, next) => {
 
     return res
       .status(200)
-      .cookie("accessToken", accessToken , options)
-      .cookie("refreshToken", newRefreshToken , options)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
       .json(
         new ApiResponse(
           200,
@@ -233,23 +233,68 @@ const refreshAccessToken = asyncHandler(async (req, res, next) => {
   }
 });
 
-const changeCurrentPassword = asyncHandler(async (req,res) => {
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?._id);
+
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invalid Old Password");
+  }
+
+  user.password = newPassword;
+  await user.save({
+    validateBeforeSave: false,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password Changd Successfully"));
+});
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current User Fetched Successfully"));
+});
+const UpdateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+
+  if (!fullName || !email) {
+    throw new ApiError(400, "All feilds are required");
+  }
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName: fullName,
+        email: email,
+      },
+    },
+    {
+      new: true,
+    }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account Details updated Successfully"));
+});
+const UpdateUserAvatar = asyncHandler(async (req, res) => {
   
-})
-const getCurrentUser = asyncHandler(async (req,res) => {
+});
 
-})
-const UpdateAccountDetails = asyncHandler(async (req,res) => {
+const UpdateUserCoverImage = asyncHandler(async (req, res) => {});
 
-})
-const UpdateUserAvatar = asyncHandler(async (req,res) => {
-
-})
-
-const UpdateUserCoverImage = asyncHandler(async (req,res) => {
-
-})
-
-
-
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  UpdateAccountDetails,
+  UpdateUserAvatar,
+  UpdateUserCoverImage,
+};
