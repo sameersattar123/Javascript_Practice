b64de3888fe12c0afa490535887f9b436bde1620
Sameer <sameersattar1111@gmail.com>
2024-02-06 02:23:00 +0500

    controllers

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -282,10 +282,65 @@ const UpdateAccountDetails = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Account Details updated Successfully"));
 });
 const UpdateUserAvatar = asyncHandler(async (req, res) => {
-  
+  const avatarLocalPath = req.file?.path
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is missing");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar.url) {
+    throw new ApiError(400, "Error while uploading on avatar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar image updated successfully"));
 });
 
-const UpdateUserCoverImage = asyncHandler(async (req, res) => {});
+const UpdateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path
+
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "Cover Image file is missing");
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!coverImage.url) {
+    throw new ApiError(400, "Error while uploading on avatar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "cover Image updated successfully"));
+
+});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {});
+
+const getWatchHistory = asyncHandler(async (req, res) => {});
 
 export {
   registerUser,
@@ -297,4 +352,6 @@ export {
   UpdateAccountDetails,
   UpdateUserAvatar,
   UpdateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 };
