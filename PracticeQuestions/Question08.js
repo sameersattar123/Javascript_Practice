4e4ed9fdca7d3900c150e6c94be371c2653114be
Sameer <sameersattar1111@gmail.com>
2024-02-17 04:14:00 +0500

    Question 08 complete

--- PracticeQuestions/Question08.js
+++ PracticeQuestions/Question08.js
@@ -1,1 +1,11 @@
-console.log("sameer sattar")
+// program that checks if the number is positive, negative or zero
+
+const num = 7;
+
+if (num > 0) {
+  console.log(`${num} is a positive number`);
+} else if (num < 0) {
+  console.log(`${num} is negative number`);
+} else {
+  console.log(`zero`);
+}
