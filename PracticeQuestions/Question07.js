f47b4c7b6cb4b2266d5be6c6e9a739923b8a6604
Sameer <sameersattar1111@gmail.com>
2024-02-17 04:09:13 +0500

    Question   07 complete

--- PracticeQuestions/Question07.js
+++ PracticeQuestions/Question07.js
@@ -1,1 +1,15 @@
-console.log("sameer sattar")
+// celsius to fahrenhiet 
+
+const celsius = 55;
+
+const fahrenhiet = celsius * 1.8 + 32;
+
+console.log(`The value of ${celsius} celsius is equals to  ${fahrenhiet} fahrenhiet`)
+
+// fahrenhiet to celsius
+
+const fahrenhiet2 = 55;
+
+const celsius2 =  (fahrenhiet - 32 ) / 1.8;
+
+console.log(`The value of ${celsius2} celsius is equals to  ${fahrenhiet2} fahrenhiet`)
