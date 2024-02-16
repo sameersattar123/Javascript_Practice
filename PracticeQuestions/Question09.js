99ea7c7205b54d8fc9ba7bc93dfed953f62df9e5
Sameer <sameersattar1111@gmail.com>
2024-02-17 04:25:17 +0500

    Question 9 and 10 complete

--- PracticeQuestions/Question09.js
+++ PracticeQuestions/Question09.js
@@ -1,1 +1,7 @@
-console.log("sameer sattar")
+const number = 90
+
+if (number % 2 === 0) {
+    console.log(`Number is Even`)
+} else {
+    console.log(`Number is odd`)
+}
--- PracticeQuestions/Question10.js
+++ PracticeQuestions/Question10.js
@@ -1,1 +1,13 @@
-console.log("sameer sattar")
+// program to find the largest among three numbers
+
+const num1 = 800;
+const num2 = 13;
+const num3 = 205;
+
+if (num3 > num2 && num3 > num1) {
+  console.log(`${num3} is a largest number among three numbers`);
+} else if (num2 > num3 && num2 > num1) {
+  console.log(`${num2} is a largest number among three numbers`);
+} else {
+  console.log(`${num1} is a largest number among three numbers`);
+}
