e8a7bfa6d80ae2ab92af10c7d4c3d5ffd5c43530
Sameer <sameersattar1111@gmail.com>
2024-02-17 02:24:35 +0500

    Question  04 complete

--- PracticeQuestions/Question04.js
+++ PracticeQuestions/Question04.js
@@ -1,1 +1,20 @@
-console.log("sameer sattar")
+// Method 01
+
+let num1 = 4;
+let num2 = 6;
+
+let temp = num1;
+num1 = num2;
+num2 = temp;
+
+console.log(`The value of num1 is ${num1} and the value of num2 is ${num2}`);
+
+// Method 02
+
+let num3 = 4;
+let num4 = 6;
+
+//using destructuring assignment
+[num3, num4] = [num4, num3];
+
+console.log(`The value of num3 is ${num3} and the value of num4 is ${num4}`);
