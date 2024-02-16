3b5e0a0efd15f6707f1f294ffc367f5510d97b04
Sameer <sameersattar1111@gmail.com>
2024-02-17 02:19:21 +0500

    question 03  complete

--- PracticeQuestions/Question03.js
+++ PracticeQuestions/Question03.js
@@ -1,1 +1,20 @@
-console.log("sameer sattar")
+// Method 01
+
+const baseValue = 4;
+const heightValue = 6;
+
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(`The Value of area is ${areaValue}`)
+
+// Method 02
+
+const side1 = 3;
+const side2 = 4;
+const side3 = 5;
+
+const s = (side1 + side2 + side3) / 2;
+
+const area = Math.sqrt((s)*(s-side1)*(s-side2)*(s-side3))
+
+console.log(`The Value of area is ${area}`)
