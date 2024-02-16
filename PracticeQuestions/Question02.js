e6a3a89eb23cd58fac6b8f275e45a1e133838ad2
Sameer <sameersattar1111@gmail.com>
2024-02-17 02:12:20 +0500

    question 2 complete

--- PracticeQuestions/Question02.js
+++ PracticeQuestions/Question02.js
@@ -1,1 +1,11 @@
-console.log("sameer sattar")
+const number1 = 16
+const number2 = 36
+const number3 = 256
+
+const sqrt1 = Math.sqrt(number1)
+const sqrt2 = Math.sqrt(number2)
+const sqrt3 = Math.sqrt(number3)
+
+console.log(`The Square Root of ${number1} is ${sqrt1}`)
+console.log(`The Square Root of ${number2} is ${sqrt2}`)
+console.log(`The Square Root of ${number3} is ${sqrt3}`)
