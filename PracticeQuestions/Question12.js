c5f0568f3ab4fc3819e994609a025b94dbbdd52d
Sameer <sameersattar1111@gmail.com>
2024-02-18 01:58:18 +0500

    Question 2 complete

--- PracticeQuestions/Question12.js
+++ PracticeQuestions/Question12.js
@@ -1,1 +1,15 @@
-console.log("sameer sattar")
+// program to find the factorial of a number
+
+const number = 5;
+
+if (number < 0) {
+  console.log("Error! Factorial for negative number does not exist.");
+} else if (number == 1) {
+  console.log(`The factorial of ${number} is 1`);
+} else {
+  let fact = 1;
+  for (let i = 1; i <= number; i++) {
+    fact *= i;
+  }
+  console.log(`The factorial of ${number} is ${fact}`);
+}
