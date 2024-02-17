2608eb3f2fd47d56d5bf879009f93fa47ba596dc
Sameer <sameersattar1111@gmail.com>
2024-02-18 02:04:55 +0500

    Question 13 complete

--- PracticeQuestions/Question13.js
+++ PracticeQuestions/Question13.js
@@ -1,1 +1,9 @@
-console.log("sameer sattar")
+// program to generate a multiplication table
+
+const number = 5;
+
+for (let i = 1; i <= 10; i++) {
+    let result = number*i
+    console.log(`${number} x ${i} = ${result} `)
+    
+}
