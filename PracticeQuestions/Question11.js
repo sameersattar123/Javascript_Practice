e031ea98f0b00b7d4079c5d47f1f112988e9f3cd
Sameer <sameersattar1111@gmail.com>
2024-02-18 01:52:46 +0500

    Question 11 complete

--- PracticeQuestions/Question11.js
+++ PracticeQuestions/Question11.js
@@ -1,1 +1,23 @@
-console.log("sameer sattar")
+// program to check if a number is prime or not
+
+const number = 23;
+let isPrime = true;
+
+if (number === 1) {
+    console.log("1 is neither a prime number nor composite number")
+} else if (number > 1) {
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+        
+    }
+
+    if (isPrime) {
+        console.log(`${number} is a prime number`)
+    } else {
+        console.log(`${number} is a not prime number`)
+    }
+}
+
