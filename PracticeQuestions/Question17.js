8f443a6a6f9889b6bf7c79faa444fd53995c97b8
Sameer <sameersattar1111@gmail.com>
2024-02-18 02:34:28 +0500

    Question 17 Complete

--- PracticeQuestions/Question17.js
+++ PracticeQuestions/Question17.js
@@ -1,1 +1,15 @@
-console.log("sameer sattar")
+// program to check whether the last digit of three numbers is same 
+
+const a = 8;
+const b = 18;
+const c = 28;
+
+const result1 = a % 10;
+const result2 = b % 10;
+const result3 = c % 10;
+
+if (result1 == result2 && result1 == result3) {
+    console.log(`${a} ${b} and ${c} have same last digit`)
+} else {
+    console.log(`${a} ${b} and ${c} have different last digit`)
+}
