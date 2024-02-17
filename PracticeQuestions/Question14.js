f169876514f1ed34e7539c063ea8f58933de70c9
Sameer <sameersattar1111@gmail.com>
2024-02-18 02:10:50 +0500

    Question 14  complete

--- PracticeQuestions/Question14.js
+++ PracticeQuestions/Question14.js
@@ -1,1 +1,13 @@
-console.log("sameer sattar")
+// program to generate fibonacci series up to n terms
+
+let number = 10
+let n0 = 0
+let n1 = 1;
+let nextTerm;
+
+for (let i = 1; i <= number; i++) {
+    console.log(n0)
+    nextTerm = n0 + n1
+    n0 = n1;
+    n1 = nextTerm
+}
