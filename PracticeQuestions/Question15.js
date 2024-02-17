8f9978844a259749a95c7114dd11c123e9d4849a
Sameer <sameersattar1111@gmail.com>
2024-02-18 02:15:45 +0500

    Question 15 complete

--- PracticeQuestions/Question15.js
+++ PracticeQuestions/Question15.js
@@ -1,1 +1,18 @@
-console.log("sameer sattar")
+// program for a simple calculator
+
+let num1 = 5;
+let num2 = 9;
+let operator = "/";
+let result;
+
+if (operator == "+") {
+ console.log(result = num1 + num2);
+} else if (operator == "-") {
+ console.log(result = num1 - num2);
+} else if (operator == "*") {
+ console.log(result = num1 * num2);
+} else if (operator == "/") {
+ console.log(result = num1 / num2);
+} else {
+  console.log("Operator does not exist");
+}
