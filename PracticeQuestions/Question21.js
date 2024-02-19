eb5fb98f8d8ae24999cdbc605548ed4c4358e0bd
Sameer <sameersattar1111@gmail.com>
2024-02-20 01:56:35 +0500

    Question 21 Complete

--- PracticeQuestions/Question21.js
+++ PracticeQuestions/Question21.js
@@ -1,1 +1,21 @@
-console.log("sameer sattar")
+// program to convert decimal to binary
+function convertIntoBinary(x){
+    let bin = 0
+    let step = 1;
+    let rem;
+    let i = 1;
+    while (x != 0) {
+        rem = x % 2;
+        console.log(
+            `Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}`
+        );
+
+        x = parseInt( x / 2)
+        bin = bin + rem * i
+        i = i*10
+    }
+    console.log(`Binary: ${bin}`);
+}
+
+const number = 9
+convertIntoBinary(number)
