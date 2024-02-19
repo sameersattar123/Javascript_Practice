cd25288b289f49ba8f2e324acc4f72de4ee636ef
Sameer <sameersattar1111@gmail.com>
2024-02-20 00:54:29 +0500

    Question 19 complete

--- PracticeQuestions/Question19.js
+++ PracticeQuestions/Question19.js
@@ -1,1 +1,12 @@
-console.log("sameer sattar")
+// program to find the sum of natural numbers using recursion
+
+function sum(num){
+    if (num > 0) {
+        return num + sum(num-1)
+    } else {
+        return num
+    }
+}
+
+const number = sum(6)
+console.log(number)
