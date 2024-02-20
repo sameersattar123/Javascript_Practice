a9409710f684e327063c3295d841183803c78caa
Sameer <sameersattar1111@gmail.com>
2024-02-20 16:00:37 +0500

    Question 30 complete

--- PracticeQuestions/Question30.js
+++ PracticeQuestions/Question30.js
@@ -1,1 +1,19 @@
-console.log("sameer sattar")
+// program to count the number of vowels in a string
+
+const vowels = ["a", "e", "i", "o", "u"]
+
+
+function countVowel(str){
+    let count = 0
+
+    for (let letter of str.toLowerCase()) {
+        if (vowels.includes(letter)) {
+            count++
+        }
+    }
+
+    return count
+}
+
+const result = countVowel("sameer")
+console.log(result)
