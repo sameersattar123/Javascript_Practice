67e55d2075774bf75ec1391f6368f9401eac7d2d
Sameer <sameersattar1111@gmail.com>
2024-02-20 15:44:36 +0500

    Question 27 complete

--- PracticeQuestions/Question27.js
+++ PracticeQuestions/Question27.js
@@ -1,6 +1,6 @@
 // program to reverse a string
 function reverseString(str){
-    let newStr = " "
+    let newStr = ""
 
     for (let i = str.length - 1; i >= 0 ; i--) {
             newStr += str[i]
