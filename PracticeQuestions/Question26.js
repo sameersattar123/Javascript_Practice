53979f432fb62c0133f30adeefdccb5ed647e903
Sameer <sameersattar1111@gmail.com>
2024-02-20 15:14:37 +0500

    Question 26 complete

--- PracticeQuestions/Question26.js
+++ PracticeQuestions/Question26.js
@@ -1,1 +1,7 @@
-console.log("sameer sattar")
+// program to replace a character of a string
+
+const str = "My name is sameer"
+
+const replaceStr = str.replace("sameer" , "sameer sattar")
+
+console.log(replaceStr)
