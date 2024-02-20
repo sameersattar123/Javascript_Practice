b1040dd3dc61406f9638be66aa0677099088138d
Sameer <sameersattar1111@gmail.com>
2024-02-20 15:13:17 +0500

    Question 25 complete

--- PracticeQuestions/Question25.js
+++ PracticeQuestions/Question25.js
@@ -1,1 +1,15 @@
-console.log("sameer sattar")
+// program to sort words in alphabetical order
+
+const sentence = "I am learning JavaScript";
+
+const words = sentence.split(" ");
+
+// console.log(words)
+
+words.sort();
+
+console.log("The sorted words are:");
+
+words.forEach((element) => {
+  console.log(element);
+});
