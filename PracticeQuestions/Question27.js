15c6be9d2af6cf0856c655c81d5610529da03bbf
Sameer <sameersattar1111@gmail.com>
2024-02-20 15:24:07 +0500

    Question 27 complete

--- PracticeQuestions/Question27.js
+++ PracticeQuestions/Question27.js
@@ -1,1 +1,14 @@
-console.log("sameer sattar")
+// program to reverse a string
+function reverseString(str){
+    let newStr = " "
+
+    for (let i = str.length - 1; i >= 0 ; i--) {
+            newStr += str[i]
+    }
+
+    return newStr
+}
+
+const string = "Sameer Sattar"
+
+console.log(reverseString(string))
