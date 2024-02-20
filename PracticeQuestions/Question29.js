a7504053908bac559afe5959d074ecdad43739bb
Sameer <sameersattar1111@gmail.com>
2024-02-20 15:44:22 +0500

    Question 29 complete

--- PracticeQuestions/Question29.js
+++ PracticeQuestions/Question29.js
@@ -1,1 +1,18 @@
-console.log("sameer sattar")
+// program to check if the string is palindrome or not
+
+function checkPalindrome(str) {
+  let newStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+
+  if (str == newStr) {
+    console.log("It is a Palindrome");
+  } else {
+    console.log("It is not a Palindrome");
+  }
+}
+
+const result  = checkPalindrome("madam")
+console.log(result)
