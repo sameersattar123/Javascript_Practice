9a74828d3518ec9423b0d39ba4d22f9d9cb987e5
Sameer <sameersattar1111@gmail.com>
2024-02-20 15:06:03 +0500

    Question 24 complete

--- PracticeQuestions/Question24.js
+++ PracticeQuestions/Question24.js
@@ -1,1 +1,16 @@
-console.log("sameer sattar")
+// program to find the factorial of a number
+
+function factorial(x){
+    if (x == 0) {
+       return 1 
+    }
+
+    return x * factorial(x-1)
+}
+
+const num = 0
+if (num < 0) {
+    console.log("Enter the Positive Number")
+} else {
+    console.log(factorial(num))
+}
