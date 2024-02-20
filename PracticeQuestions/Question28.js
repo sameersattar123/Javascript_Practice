dea793a72622fd61ef649420635f3f2a4242b71f
Sameer <sameersattar1111@gmail.com>
2024-02-20 15:30:41 +0500

    Question 28 complete

--- PracticeQuestions/Question28.js
+++ PracticeQuestions/Question28.js
@@ -1,1 +1,16 @@
-console.log("sameer sattar")
+function countStr(str , letter){
+
+    let count = 0
+
+    for (let i = 0; i < str.length; i++) {
+           
+        if (str.charAt(i) == letter) {
+            count ++
+        }
+    }
+
+    return count
+}
+
+const result = countStr("sameer sattar" , "a")
+console.log(result)
