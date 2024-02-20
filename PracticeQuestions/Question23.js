40e69a7f4f7ac6ab3b7aad1bf457afa986589d02
Sameer <sameersattar1111@gmail.com>
2024-02-20 15:02:43 +0500

    Question 23 complete

--- PracticeQuestions/Question23.js
+++ PracticeQuestions/Question23.js
@@ -1,1 +1,18 @@
-console.log("sameer sattar")
+// program to display fibonacci sequence using recursion
+
+function fibonacci(num) {
+  if (num < 2) {
+    return num;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+const number = fibonacci(8);
+if (number <= 0) {
+  console.log("Enter a positive integer.");
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log(fibonacci(i));
+  }
+}
