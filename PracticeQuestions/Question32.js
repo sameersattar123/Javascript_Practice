17fd6270bce1f931766e4363e2a94680461ba0cc
Sameer <sameersattar1111@gmail.com>
2024-02-21 17:46:01 +0500

    Question 32 Complete

--- PracticeQuestions/Question32.js
+++ PracticeQuestions/Question32.js
@@ -1,1 +1,24 @@
-console.log("sameer sattar")
+// program to create JavaScript object using object literal
+const person = {
+    name : "Sameer Sattar",
+    age : 20,
+    hobbies : ["reading" , "dancing" , "coding" , "gaming" , "teaching"],
+    greet : function (){
+        console.log("Helloe EveryOne")
+    },
+    score : {
+        math : 23,
+        phy : 22,
+        chem : 22
+    }
+}
+
+console.log(typeof person)
+
+console.log(person)
+
+delete person.score
+
+delete person.greet
+
+console.log(person)
