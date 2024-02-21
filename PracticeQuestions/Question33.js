b342a646a8de7bd3cbef0823228b85ee54752586
Sameer <sameersattar1111@gmail.com>
2024-02-21 17:50:57 +0500

    Question 33 Complete

--- PracticeQuestions/Question33.js
+++ PracticeQuestions/Question33.js
@@ -1,1 +1,13 @@
-console.log("sameer sattar")
+function checkString(str){
+    if (str.startsWith("s") && str.endsWith("r")) {
+        console.log("Your name start with s and end with r")
+    } else if(str.startsWith("s")){
+        console.log("Your name start with s")
+    } else if (str.endsWith("r")){
+        console.log("Your name end with r")
+    } else {
+        console.log('The string does not start with s and does not end with r');
+    }
+}
+
+checkString("sameer")
