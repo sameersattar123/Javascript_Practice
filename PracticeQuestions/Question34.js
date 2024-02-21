b0b8efbdc4b08716f58669c90271ef9d88df54be
Sameer <sameersattar1111@gmail.com>
2024-02-21 17:56:32 +0500

    Question 34 complete

--- PracticeQuestions/Question34.js
+++ PracticeQuestions/Question34.js
@@ -1,1 +1,29 @@
-console.log("sameer sattar")
+// program to check if a key exists
+
+const person1 = {
+    id: 1,
+    name: 'John',
+    age: 23
+}
+
+let hasKey = "name" in person1
+
+if (hasKey) {
+    console.log("The key exists")
+} else {
+    console.log('The key does not exist.');
+}
+
+// method 2
+
+const person2 = {
+    id: 1,
+    name: 'John',
+    age: 23
+}
+
+if (person2.hasOwnProperty("name")) {
+    console.log('The key exist.');
+} else {
+    console.log('The key does not exist.');
+}
