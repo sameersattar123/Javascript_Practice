b128fe1904e4512cea1500bc662704e46ccc5908
Sameer <sameersattar1111@gmail.com>
2024-02-22 19:22:24 +0500

    Question 35 completed

--- PracticeQuestions/Question35.js
+++ PracticeQuestions/Question35.js
@@ -1,1 +1,18 @@
-console.log("sameer sattar")
+// program to clone the object
+// declaring object
+
+const person = {
+    name: 'John',
+    age: 21,
+}
+
+let clonePerson = {...person}
+
+console.log(person)
+console.log(clonePerson)
+
+clonePerson.name = "sameer sattar"
+clonePerson.age = 67;
+
+console.log(person.name)
+console.log(clonePerson.name)
