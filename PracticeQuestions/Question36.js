cf041861363db226c9ed12eb2d4bdc4403eb7c01
Sameer <sameersattar1111@gmail.com>
2024-02-22 19:37:32 +0500

    Question 36 completed

--- PracticeQuestions/Question36.js
+++ PracticeQuestions/Question36.js
@@ -1,1 +1,25 @@
-console.log("sameer sattar")
+// program to loop through an object using for...in loop
+
+const student = {
+  name: "John",
+  age: 20,
+  hobbies: ["reading", "games", "coding"],
+};
+
+for (let key in student) {
+  // console.log(key)
+  let values = student[key];
+  // console.log(values)
+  console.log(`${key} - ${values} `);
+}
+
+
+const student2 = { 
+    name: 'John',
+    age: 20,
+    hobbies: ['reading', 'games', 'coding'],
+};
+
+for (const [key , value] of Object.entries(student2)) {
+    console.log(`${key} - ${value}`)
+}
