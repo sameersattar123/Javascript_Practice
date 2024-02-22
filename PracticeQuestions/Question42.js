4276f2a695c2f0473c65996b5c45c11b674e4f23
Sameer <sameersattar1111@gmail.com>
2024-02-22 21:00:26 +0500

    Question 42 complete

--- PracticeQuestions/Question42.js
+++ PracticeQuestions/Question42.js
@@ -1,1 +1,23 @@
-console.log("sameer sattar")
+// program to generate random strings
+
+// declare all characters
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateRandomString(length){
+    let result = ''
+    let charLength = characters.length
+
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charLength))
+    }
+
+    return result
+}
+
+const result = generateRandomString(5)
+console.log(result)
+
+// Method 2
+
+const result2 = Math.random().toString(36).substring(2,10)
+console.log(result2)
