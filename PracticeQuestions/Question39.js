bead7890d33b81235071fe6078e2b9f0adb2510f
Sameer <sameersattar1111@gmail.com>
2024-02-22 20:00:10 +0500

    Question  39 completed

--- PracticeQuestions/Question39.js
+++ PracticeQuestions/Question39.js
@@ -1,1 +1,13 @@
-console.log("sameer sattar")
+// program to add a key/value pair to an object
+
+const person = {
+    name: 'Monica',
+    age: 22,
+    gender: 'female'
+}
+
+// add a key/value pair
+person.height = 5.4;
+person['weight'] = 52.4;
+
+console.log(person);
