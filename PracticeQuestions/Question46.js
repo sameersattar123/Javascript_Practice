445f49a948629e346e03c5b75ad0d6ddf176b406
Sameer <sameersattar1111@gmail.com>
2024-02-22 21:22:01 +0500

    Question 46 complete

--- PracticeQuestions/Question46.js
+++ PracticeQuestions/Question46.js
@@ -1,1 +1,12 @@
-console.log("sameer sattar")
+// js program to perform string comparison
+
+const string1 = 'JavaScript Program';
+const string2 = 'javascript program';
+
+const result = string1.toUpperCase() === string2.toUpperCase()
+
+if(result) {
+    console.log('The strings are similar.');
+} else {
+    console.log('The strings are not similar.');
+}
