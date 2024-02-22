65e0ce59310d8918a52fcb53bd8cc1c7f8d9ffa0
Sameer <sameersattar1111@gmail.com>
2024-02-22 21:07:40 +0500

    Question 43 complete

--- PracticeQuestions/Question43.js
+++ PracticeQuestions/Question43.js
@@ -1,1 +1,10 @@
-console.log("sameer sattar")
+const string = "sameer Sattar"
+
+const toCheckString = 'sa'
+
+if (string.startsWith(toCheckString)) {
+    console.warn('The string starts with "sa".');
+}
+else {
+    console.warn(`The string does not starts with "sa".`);
+}
