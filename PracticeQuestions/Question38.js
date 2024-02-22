68eddd5c1d08f69e95bb6d51be81cf3313888676
Sameer <sameersattar1111@gmail.com>
2024-02-22 19:58:42 +0500

    Question 38 completed

--- PracticeQuestions/Question38.js
+++ PracticeQuestions/Question38.js
@@ -1,1 +1,17 @@
-console.log("sameer sattar")
+// program to count the number of keys/properties in an object
+
+const student = {
+  name: "John",
+  age: 20,
+  hobbies: ["reading", "games", "coding"],
+};
+
+let count = 0;
+
+for (const key in student) {
+  ++count;
+}
+console.log(count);
+
+const result = Object.keys(student).length; // the Object.keys() method and the length property are used to count the number of keys in an object.
+console.log(result);
