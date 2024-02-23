bb66c630f7aa9de579534185077366264d43ae2e
Sameer <sameersattar1111@gmail.com>
2024-02-24 00:56:26 +0500

    Question 48 complete

---
+++ PracticeQuestions/Question48.js
@@ -0,0 +1,11 @@
+// program to check leap year
+
+function checkLeapYear(year){
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    }else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+checkLeapYear(2000)
