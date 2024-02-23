a8bc1b82ee94110db3a04e8f0ae446b7d62f52a9
Sameer <sameersattar1111@gmail.com>
2024-02-24 00:56:15 +0500

    Question 47 complete

--- PracticeQuestions/Question47.js
+++ PracticeQuestions/Question47.js
@@ -1,1 +1,9 @@
-console.log("sameer sattar shaikh")
+// program to display the date and time
+
+const date = new Date()
+
+const n = date.toDateString()
+
+const time = date.toLocaleTimeString()
+
+console.log(`Date: ${n} and Time: ${time}`)
