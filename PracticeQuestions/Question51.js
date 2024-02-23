98ffc82f2989facd80bd3be2f09b912638780517
Sameer <sameersattar1111@gmail.com>
2024-02-24 01:06:17 +0500

    Question 51 complete

--- PracticeQuestions/Question51.js
+++ PracticeQuestions/Question51.js
@@ -1,1 +1,26 @@
-console.log("sameer")
+// program to compare value of two dates
+// create two dates
+const d1 = new Date();
+console.log(d1)
+
+const d2 = new Date();
+console.log(d2)
+
+// comparisons
+const compare1 = d1 < d2
+console.log(compare1)
+
+const compare2 = d1 > d2
+console.log(compare2)
+
+const compare3 = d1 <= d2
+console.log(compare3)
+
+const compare4 = d1 >= d2
+console.log(compare4)
+
+const compare5 = d1.getTime() === d2.getTime()
+console.log(compare5)
+
+const compare6 = d1.getTime() !== d2.getTime()
+console.log(compare6)
