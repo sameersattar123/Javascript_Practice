1d779f98b0a82b69f2e0de1e2c5fe0efa1984443
Sameer <sameersattar1111@gmail.com>
2024-02-24 00:56:39 +0500

    Question 49 complete

---
+++ PracticeQuestions/Question49.js
@@ -0,0 +1,9 @@
+// program to replace all instances of character in a string
+
+const string = 'Learning JavaScript Program';
+
+const splitString = string.split('a')
+
+const result = splitString.join('A');
+
+console.log(result)
