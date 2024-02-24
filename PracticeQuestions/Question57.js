0deaef70c02e704911d85c5a4667f76ca123358c
Sameer <sameersattar1111@gmail.com>
2024-02-24 13:31:43 +0500

    Question 57 complete

--- PracticeQuestions/Question57.js
+++ PracticeQuestions/Question57.js
@@ -1,1 +1,14 @@
-console.log("sameer")
+// program to check if an object is an array
+
+function checkArray(arr){
+
+    const result = Array.isArray(arr)
+    if(result) {
+        console.log(`[${arr}] is an array.`);
+    }
+    else {
+        console.log(`${arr} is not an array.`);
+    }
+}
+
+checkArray([1,2,3,4])
