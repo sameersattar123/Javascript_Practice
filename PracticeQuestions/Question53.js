16e42f94cb31959019021ebc72cd40803c1fddf5
Sameer <sameersattar1111@gmail.com>
2024-02-24 12:35:38 +0500

    Question 53 complete

--- PracticeQuestions/Question53.js
+++ PracticeQuestions/Question53.js
@@ -1,1 +1,17 @@
-console.log("sameer")
+// program to remove item from an array
+
+function removeElementFromArray(array, n) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== n) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+let newArray = removeElementFromArray(
+  [1, 2, 3, 4, 5, 7, 8, 9, 5, 3, 2, 1, 1, 3, 43, 5, 6, 6, 6],
+  6
+);
+console.log(newArray);
