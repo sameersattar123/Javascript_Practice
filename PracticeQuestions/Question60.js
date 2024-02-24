044a2237a96cde49aeeb68ca4788e4b600f300e2
Sameer <sameersattar1111@gmail.com>
2024-02-24 14:10:39 +0500

    Question 60 complete

--- PracticeQuestions/Question60.js
+++ PracticeQuestions/Question60.js
@@ -1,1 +1,16 @@
-console.log("sameer")
+// program to remove duplicate value from an arra
+
+function getUnique(arr){
+    let unique = [];
+    for (i = 0; i < arr.length; i++) {
+        if (unique.indexOf(arr[i]) === -1) {
+            unique.push(arr[i]);
+        }
+    }
+    return unique;
+}
+
+const array = [1,2,3,4,5,6,7,8,9,3,2,1,4,5,6 ,5,5,5,5,5 ,10]
+console.log(array)
+const newArray = getUnique(array)
+console.log(newArray)
