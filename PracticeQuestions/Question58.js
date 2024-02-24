e2ac30001d568ff4e911e01e6c51a03e1d574716
Sameer <sameersattar1111@gmail.com>
2024-02-24 13:36:16 +0500

    Question 58 complete

--- PracticeQuestions/Question58.js
+++ PracticeQuestions/Question58.js
@@ -1,1 +1,12 @@
-console.log("sameer")
+// program to empty an array
+
+function emptyArray(array){
+    // array.length = 0
+    array = []
+    return array
+}
+
+const array = [1,2,3,4,5,6]
+console.log(array)
+const newArray = emptyArray(array)
+console.log(newArray)
