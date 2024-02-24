4996d271348d9824d32260ccc62949a1a1321433
Sameer <sameersattar1111@gmail.com>
2024-02-24 12:49:18 +0500

    Question 54 complete

--- PracticeQuestions/Question54.js
+++ PracticeQuestions/Question54.js
@@ -1,1 +1,14 @@
-console.log("sameer")
+// program to check if an array contains a specified value
+
+function checkSpecificValue(array , value){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == value) {
+            console.log('Array contains a value.');
+            break
+        } else {
+            console.log('Array does not contain a value.');
+        } 
+    }
+}
+
+checkSpecificValue([1,2,3,4,5,6] ,6)
