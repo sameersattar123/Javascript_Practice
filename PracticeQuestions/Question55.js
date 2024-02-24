cf69c617944508a630157b298c72f6c6891c4f73
Sameer <sameersattar1111@gmail.com>
2024-02-24 13:23:27 +0500

    Question 55 complete

--- PracticeQuestions/Question55.js
+++ PracticeQuestions/Question55.js
@@ -1,1 +1,12 @@
-console.log("sameer")
+// program to insert an item at a specific index into an array
+
+function insertElement(array , index , element){
+    for (let i = array.length; i > index; i--) {
+        array[i] = array[i-1]
+    }
+    array[index] = element
+    return array
+}
+
+const newArray = insertElement([1,2,3,4,5] , 5 ,6)
+console.log(newArray)
