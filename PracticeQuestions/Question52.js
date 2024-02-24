3e22ce9473ea96e0738f7f1dbc9b78b3aa1bc94b
Sameer <sameersattar1111@gmail.com>
2024-02-24 12:26:26 +0500

    Question 52 complete

--- PracticeQuestions/Question52.js
+++ PracticeQuestions/Question52.js
@@ -1,1 +1,22 @@
-console.log("sameer")
+// program to create a countdown timer
+
+let countDownTime = new Date().getTime() + 24 * 60 * 60 * 1000;
+
+// console.log(countDownTime);
+
+let x = setInterval(() => {
+  let now = new Date().getTime();
+  let timeLeft = countDownTime - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+  const seconds = Math.floor((timeLeft / 1000) % 60);
+
+  console.log(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+
+  if (timeLeft < 0) {
+    clearInterval(x);
+    console.log("CountDown Finished");
+  }
+}, 1000);
