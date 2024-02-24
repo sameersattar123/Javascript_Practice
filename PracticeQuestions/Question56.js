5f87c1e8e6be95d75726b53b07d3ab9495a7fbca
Sameer <sameersattar1111@gmail.com>
2024-02-24 13:27:15 +0500

    Question 56 complete

--- PracticeQuestions/Question56.js
+++ PracticeQuestions/Question56.js
@@ -1,1 +1,13 @@
-console.log("sameer")
+// program to append an object to an array
+
+function insertObject(arr , obj){
+    // arr.push(obj); // another way to insert object into array
+    arr = [...arr , obj]
+    return arr
+}
+
+const newArray = insertObject([1,2,3,4,5] , {
+    name : "sameer"
+})
+
+console.log(newArray)
