9c9fcc2462e3d509c03c0aeb1cb5ae3a836474e7
Sameer <sameersattar1111@gmail.com>
2024-02-24 13:41:28 +0500

    Question 59 complete

--- PracticeQuestions/Question59.js
+++ PracticeQuestions/Question59.js
@@ -1,1 +1,11 @@
-console.log("sameer")
+// program to add element to an array
+
+function addElement(array , element){
+    array = [element , ...array]
+    return array
+}
+
+const array = [1,2,3,4,5,6]
+console.log(array)
+const newArray = addElement(array , 0)
+console.log(newArray)
