1f219bb0629573a14ee2c6ccad7721043af268aa
Sameer <sameersattar1111@gmail.com>
2024-02-28 23:44:09 +0500

    some changes

--- Projects/FoodRecipe/src/App.jsx
+++ Projects/FoodRecipe/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home/Home"
+import Favorite from "./pages/Favorite/Favorite"
+import Details from "./pages/Details/Details"
+import Navbar from "./components/Navbar/Navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="min-h-screen text-lg text-gray-600 p-6 bg-white">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/favorites' element={<Favorite/>} />
+        <Route path='/recipe-item/:id' element={<Details/>} />
+      </Routes>
+    </div>
   )
 }
 
