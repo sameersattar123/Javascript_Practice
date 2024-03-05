ba5d66e5141f1f6373e057e993bba45b153ac9d5
Sameer <sameersattar1111@gmail.com>
2024-03-06 03:38:18 +0500

    Question 75 complete

--- PracticeQuestions/Question75.js
+++ PracticeQuestions/Question75.js
@@ -1,1 +1,49 @@
-console.log("sameer sattar")
+// program to implement stack data structure
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  add(element) {
+    return this.items.push(element);
+  }
+
+  remove() {
+    if (this.items.length > 0) {
+      return this.items.pop();
+    }
+  }
+
+  lastElement() {
+    return this.items[this.items.length-1];
+  }
+  isEmpty() {
+    return this.items.length == 0;
+  }
+  size() {
+    return this.items.length;
+  }
+  clear() {
+    this.items = [];
+  }
+}
+
+let stack = new Stack();
+stack.add(3);
+stack.add(2);
+stack.add(22);
+stack.add(44);
+console.log(stack.items);
+
+stack.remove();
+console.log(stack.items);
+
+console.log(stack.lastElement());
+
+console.log(stack.isEmpty());
+
+console.log(stack.size());
+
+stack.clear();
+console.log(stack.items);
