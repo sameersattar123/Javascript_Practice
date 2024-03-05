e8d4105f0a4bdbfdc38685f645bf6b5eb357376e
Sameer <sameersattar1111@gmail.com>
2024-03-06 02:40:31 +0500

    Question 71 complete

--- PracticeQuestions/Question71.js
+++ PracticeQuestions/Question71.js
@@ -1,1 +1,18 @@
-console.log("sameer sattar")
+// program to pass parameter to a setTimeout() function
+
+function greet(){
+    console.log("Hello Everyone")
+}
+
+console.log('This message is shown first'); // run first
+setTimeout(greet ,3000) // run after console.log
+
+
+// program to pass parameter to function in setTimeout()
+function greet2(x, y) {
+    console.log(x);
+    console.log(y);
+}
+
+console.log('This message is shown first'); // run first
+setTimeout(greet2 , 2000 , "sameer" ,"sattar")
