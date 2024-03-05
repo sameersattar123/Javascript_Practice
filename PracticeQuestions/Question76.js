f9cfc46f742a1be498f51f917fff36f135a2a445
Sameer <sameersattar1111@gmail.com>
2024-03-06 04:23:41 +0500

    incomplete

--- PracticeQuestions/Question76.js
+++ PracticeQuestions/Question76.js
@@ -1,1 +1,58 @@
-console.log("sameer sattar")
+// program to implement queue data structure
+class Queue {
+    constructor(){
+        this.items = {};
+        this.headIndex = 0
+        this.lastIndex = 0
+    }
+
+    //adds a new element
+    enqueue(){
+        
+    }
+    //removes an element from head of the queue
+    dequeue(){
+
+    }
+
+    size(){
+
+    }
+    isEmpty(){
+
+    }
+    lastElement(){
+
+    }
+
+    clear(){
+
+    }
+}
+
+
+let queue = new Queue();
+
+// add items to queue
+queue.enqueue(8);
+queue.enqueue(6);
+queue.enqueue(4);
+queue.enqueue(2);
+
+console.log("Queue after adding items: ");
+console.log(queue.items);
+
+// remove the first item
+queue.dequeue();
+
+console.log("Queue after deleting the first item:");
+console.log(queue.items);
+
+// show the first item
+console.log("First item of the queue = " + queue.lastElement());
+
+// empty the queue
+queue.clear();
+
+console.log("After clearing the queue: ");
+console.log(queue.items);
