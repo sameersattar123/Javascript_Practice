d8b01b8bb7f442c56e139eae314d0b642d794842
Sameer <sameersattar1111@gmail.com>
2024-03-05 18:40:18 +0500

    Question 70 complete

--- PracticeQuestions/Question70.js
+++ PracticeQuestions/Question70.js
@@ -4,3 +4,13 @@ const min = 2;
 const randomValues = Math.floor(Math.random()* (max-min +1)) + min
 
 console.log(`Random value between ${min} and ${max} is ${randomValues}`);
+
+// In JavaScript, you can generate a random number with the Math.random() function.
+
+// Math.random() returns a random floating-point number ranging from 0 to less than 1 (inclusive of 0 and exclusive of 1)
+
+// The above program will show an integer output between min (inclusive) to max (inclusive).
+
+// First, the minimum and maximum values are taken as input from the user. Then the Math.random() method is used to get the random number from the passed value.
+
+// The Math.floor() returns the nearest integer value.
