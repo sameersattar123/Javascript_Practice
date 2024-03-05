b4d460733a4e43840f71707ff54b80d81d383e48
Sameer <sameersattar1111@gmail.com>
2024-03-05 18:26:46 +0500

    Question 67 complete

--- PracticeQuestions/Question67.js
+++ PracticeQuestions/Question67.js
@@ -1,1 +1,8 @@
-console.log("sameer")
+import {number , message ,multiplyNumbers} from "./Question66";
+
+console.log(message); // hello world
+
+console.log(number); // 10
+
+console.log(multiplyNumbers(3, 4)); // 12
+console.log(multiplyNumbers(5, 8)); // 40
