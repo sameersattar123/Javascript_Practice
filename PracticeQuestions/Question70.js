01b705309886f21f26ac5e79bf4d34bf1d271440
Sameer <sameersattar1111@gmail.com>
2024-03-05 18:37:54 +0500

    Question 70 complete

--- PracticeQuestions/Question70.js
+++ PracticeQuestions/Question70.js
@@ -1,1 +1,6 @@
-console.log("sameer")
+const max = 20;
+const min = 2;
+
+const randomValues = Math.floor(Math.random()* (max-min +1)) + min
+
+console.log(`Random value between ${min} and ${max} is ${randomValues}`);
