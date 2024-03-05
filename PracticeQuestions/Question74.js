55ec3094501fdfd9bcbe6e60607a6197f2826c09
Sameer <sameersattar1111@gmail.com>
2024-03-06 03:02:58 +0500

    Question 74 complete

--- PracticeQuestions/Question74.js
+++ PracticeQuestions/Question74.js
@@ -1,1 +1,12 @@
-console.log("sameer sattar")
+// program to pass a function as a parameter
+
+function greet(){
+    return "Hello"
+}
+
+function anotherGreet(user,func){
+    let message = func()
+    console.log(`${message} ${user}`)
+}
+
+anotherGreet("sameer sattar" ,greet)
