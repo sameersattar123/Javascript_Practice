103c1a049bc5a24551dac162e0476e55cbfe5945
Sameer <sameersattar1111@gmail.com>
2024-03-05 18:32:16 +0500

    Question 69 complete

--- PracticeQuestions/Question69.js
+++ PracticeQuestions/Question69.js
@@ -1,1 +1,10 @@
-console.log("sameer")
+// program to set default parameter value
+
+function defaultParameter(x = 3 , y = 5){
+     console.log(x * y)
+}
+
+defaultParameter(2,3)
+defaultParameter()
+defaultParameter(4,3)
+defaultParameter()
