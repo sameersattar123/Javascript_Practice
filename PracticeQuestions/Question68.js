896570282ded37f6e9d6c2824284ec3afa51303e
Sameer <sameersattar1111@gmail.com>
2024-03-05 18:29:45 +0500

    Question 68 complete

--- PracticeQuestions/Question68.js
+++ PracticeQuestions/Question68.js
@@ -1,1 +1,14 @@
-console.log("sameer")
+// program to check if a variable is undefined or null
+
+function  checkNull(variable){
+    if (variable == null) {
+        console.log("The variable is undefined or null")
+    }else {
+        console.log("The variable is neither undefined nor null")
+    }
+}
+
+checkNull(undefined)
+checkNull(4)
+checkNull("HELLO")
+checkNull(null)
