82ad5ab67efe959e91b01c3c7910a75f19c15402
Sameer <sameersattar1111@gmail.com>
2024-03-06 02:49:49 +0500

    Question 72 complete

--- PracticeQuestions/Question72.js
+++ PracticeQuestions/Question72.js
@@ -1,1 +1,21 @@
-console.log("sameer sattar")
+// program to perform function overloading
+
+function sum() {
+  if (arguments.length == 0) {
+    console.log("You have not passed any argument");
+  } else if (arguments.length == 1) {
+    console.log("Pass at least two arguments");
+  } else {
+    let result = 0;
+    let arg = arguments.length;
+    for (let i = 0; i < arg; i++) {
+      result = result + arguments[i];
+    }
+    return result;
+  }
+}
+
+console.log(sum(2, 3,5));
+console.log(sum(2, 3));
+console.log(sum(2));
+console.log(sum());
