f2bdda2ad0d5d65c3c70ed0cf5aacb66ad0cc6ad
Sameer <sameersattar1111@gmail.com>
2024-03-05 18:26:37 +0500

    Question 66  complete

--- PracticeQuestions/Question66.js
+++ PracticeQuestions/Question66.js
@@ -1,1 +1,11 @@
-console.log("sameer")
+// program to include JS file into another JS file
+
+const message = 'hello world';
+
+const number = 10;
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+export {message ,number ,multiplyNumbers}
