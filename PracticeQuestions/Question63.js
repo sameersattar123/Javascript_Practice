dbbc8355fcd3cc60dd645f032a8599171a9125db
Sameer <sameersattar1111@gmail.com>
2024-03-05 15:26:22 +0500

    Question 63 complete

--- PracticeQuestions/Question63.js
+++ PracticeQuestions/Question63.js
@@ -1,1 +1,13 @@
-console.log("sameer")
+// program to extract value as an array from an array of objects
+
+function extractValue(arr , prop){
+    const exactValue = arr.map((item) => item[prop])
+
+    return exactValue
+} 
+
+const objArray = [{a: 1, b: 2}, {a: 4, b: 5}, {a: 8, b: 9}];
+
+// passing an array of objects and property 'a' to extract
+const result = extractValue(objArray, 'b');
+console.log(result);
