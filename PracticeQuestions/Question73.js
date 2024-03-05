494f6bc01d71ac5ad72b4d543f0463840c3bb6d8
Sameer <sameersattar1111@gmail.com>
2024-03-06 02:57:55 +0500

    Question 73 complete

--- PracticeQuestions/Question73.js
+++ PracticeQuestions/Question73.js
@@ -1,1 +1,17 @@
-console.log("sameer sattar")
+// program to check if a number is a float or integer value
+
+function checkNumber(x) {
+  if (typeof x == "number" && !isNaN(x)) {
+    if (Number.isInteger(x)) {
+      console.log(`${x} is integer.`);
+    } else {
+      console.log(`${x} is a float value.`);
+    }
+  } else {
+    console.log(`${x} is not a number`);
+  }
+}
+
+checkNumber(3)
+checkNumber(3.5)
+checkNumber("Hello World")
