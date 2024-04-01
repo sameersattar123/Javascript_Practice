071dce817b613519c11fda5dc066444aaaa4e9a0
Sameer <sameersattar1111@gmail.com>
2024-04-01 05:06:40 +0500

    starting

---
+++ Projects/ShoppingCard/src/components/CartTitle/CartTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartTitle = () => {
+  return (
+    <div>CartTitle</div>
+  )
+}
+
+export default CartTitle
---
+++ Projects/ShoppingCard/src/components/ProductTitle/ProductTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductTitle = () => {
+  return (
+    <div>ProductTitle</div>
+  )
+}
+
+export default ProductTitle
