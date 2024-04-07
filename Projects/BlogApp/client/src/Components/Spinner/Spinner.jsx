fe00652d447929daff1d82209c8733210b9ead64
Sameer <sameersattar1111@gmail.com>
2024-04-08 03:12:15 +0500

    components and pages

---
+++ Projects/BlogApp/client/src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div>Spinner</div>
+  )
+}
+
+export default Spinner
---
+++ Projects/BlogApp/client/src/Pages/AddBlog/AddBlog.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./AddBlog.css"
+
+const AddBlog = () => {
+  return (
+    <div>AddBlog</div>
+  )
+}
+
+export default AddBlog
