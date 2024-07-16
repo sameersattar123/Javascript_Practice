02566119a8022d9567f395ecfe580f34b55a0c3a
Sameer <sameersattar1111@gmail.com>
2024-07-16 12:46:49 +0500

    complete

---
+++ Basics02/05.js
@@ -0,0 +1,41 @@
+// type of opertors
+
+// data types (primitives data types)
+
+// booleans 
+// strings
+// numbers
+// BigInt
+// symbol
+// undefined
+// null
+
+let firstName = "waniya"
+
+let age = 67;
+
+let memory;
+
+let box = ' '
+
+console.log(typeof firstName)  // strings
+console.log(typeof age) // number 
+console.log(typeof memory) // undefined
+
+// convert number to string 
+
+let num = 45
+num = num + ''
+console.log(typeof num)
+
+let num2 = String(55);
+console.log(typeof num2)
+
+
+// convert string to number
+
+let myStr = +'sameer'
+console.log(typeof myStr)
+
+let myStr2 = Number('45')
+console.log(typeof myStr2)
---
+++ Basics02/06.js
@@ -0,0 +1,22 @@
+// strings concatenation
+
+let str1 = 'sameer'
+let str2 = 'sattar'
+
+let fullName = str1 + ' ' + str2
+console.log(fullName)
+console.log(typeof fullName)
+
+let num1 = "44"
+let num2  = '55';
+
+let totalSum = +num1 + +num2
+console.log(totalSum)
+console.log(typeof totalSum)
+
+let str3 = "23.5"
+let str4 = "dd"
+
+let sum = str3.concat(str4)
+console.log(sum)
+console.log(typeof sum)
