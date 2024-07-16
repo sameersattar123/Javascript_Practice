9c86a0023406942800c0c626c46e525a3a6ab482
Sameer <sameersattar1111@gmail.com>
2024-07-16 10:55:59 +0500

    complete

---
+++ Basics02/01.js
@@ -0,0 +1,36 @@
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+
+var firstName = "sameer";
+
+// use  a variable
+
+console.log(firstName);
+
+// change the value of a variable
+
+firstName = "sattar";
+
+console.log(firstName);
+
+// rules for naming variables 
+
+// you cannot start with number 
+// example :- 
+// 1value (invalid)
+// value1 (valid)
+
+const value1 = 20;
+console.log(value1**0.5);
+
+const first_name = 'sameer'; // snake case writing
+console.log(first_name)
+
+const secondName = 'Sameer'; // camel case writing
+console.log(secondName)
+
