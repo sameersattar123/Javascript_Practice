60d7524e10686248945fb88e3e9941e97f1a35e3
Sameer <sameersattar1111@gmail.com>
2024-07-16 12:49:41 +0500

    complete

--- Basics02/07.js
+++ Basics02/07.js
@@ -1,2 +1,13 @@
 // strings template
 
+let name = "sameer sattar"
+
+let age = 66;
+
+let info = "my name is " + name  + " and my age is " + age
+
+console.log(info)
+
+let info2 = `my name is ${name} and my age is ${age}`
+
+console.log(info2)
