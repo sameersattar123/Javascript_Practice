f4aea024c9972df05afee5f9289d7600c69fcd44
Sameer <sameersattar1111@gmail.com>
2024-07-24 19:10:45 +0500

    complete

--- Basics02/14.js
+++ Basics02/14.js
@@ -59,3 +59,49 @@ function outerFunction() {
 }
 
 outerFunction()
+
+
+// block vs function scope
+
+{
+  let a = 880;
+  console.log("Inner a " , a); // 880
+  const b = 9990;
+  console.log(b) // 9990
+  var c = 40;
+  console.log(c) // 40
+}
+
+// let and const has block scope
+
+// console.log(a); // a is not defined
+// console.log(b) // b is not defined
+
+// var has function scope
+console.log(c)  // 40  
+
+// default prameters
+
+function multiply(a = 4, b = 12) {
+  return a * b;
+}
+
+console.log(multiply(2, 3)); // 6
+console.log(multiply(2)); // 24
+console.log(multiply()) // 48
+
+// rest parameters
+
+function addAll(...nums) {
+  // console.log(Array.isArray(nums)) // true
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return sum;
+}
+console.log(addAll(1,2,3,4,5,6,7,8,9,10))
+
+// param destructing
+
+
